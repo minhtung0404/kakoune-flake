2308be931682fe6d0bf70f5ae376a0dd5487f21d
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-07-29 13:27:17 +0200

    fix: overlay -> overlays

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
         # The usual flake attributes can be defined here, including system-
         # agnostic ones like nixosModule and system-enumerating ones, although
         # those are more easily expressed in perSystem.
-        overlay.default = nixpkgs.lib.composeManyExtensions (import ./flake/overlays.nix inputs);
+        overlays.default = nixpkgs.lib.composeManyExtensions (import ./flake/overlays.nix inputs);
       };
     };
 }
